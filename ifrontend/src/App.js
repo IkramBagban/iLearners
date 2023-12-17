37edf4fae4824abbe278a60b3fe4d9e2f95f1e0a
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-12-17 16:37:00 +0530

    added some new sheets

--- ifrontend/src/App.js
+++ ifrontend/src/App.js
@@ -1,40 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import Sheet from './components/Sheet';
+import logo from "./logo.svg";
+import "./App.css";
+import Sheet from "./components/Sheet";
 
 function App() {
-
-
-  
-
   return (
     <>
-    <Sheet />
-    <div className="App">
-      <table className="border-collapse border border-slate-400 ...">
-  <thead>
-    <tr>
-      <th className="border border-slate-300 ...">State</th>
-      <th className="border border-slate-300 ...">City</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td className="border border-slate-300 ...">Indiana</td>
-      <td className="border border-slate-300 ...">Indianapolis</td>
-    </tr>
-    <tr>
-      <td className="border border-slate-300 ...">Ohio</td>
-      <td className="border border-slate-300 ...">Columbus</td>
-    </tr>
-    <tr>
-      <td className="border border-slate-300 ...">Michigan</td>
-      <td className="border border-slate-300 ...">Detroit</td>
-    </tr>
-  </tbody>
-</table>
-</div>
-    
+      <Sheet />
     </>
   );
 }
--- ifrontend/src/components/Sheet.jsx
+++ ifrontend/src/components/Sheet.jsx
@@ -1,8 +1,8 @@
-// import React from 'react';
-import useFetch from '../customHooks/useFetch';
+import React from "react";
+import useFetch from "../customHooks/useFetch";
 
 const Sheet = () => {
-  const { data, isLoading, isError } = useFetch('/fraz');
+  const { data, isLoading, isError } = useFetch("/fraz");
 
   if (isError) {
     return <div>Error fetching data.</div>;
@@ -13,42 +13,51 @@ const Sheet = () => {
   }
 
   return (
-    <div>
-      <div className="overflow-x-auto relative">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="py-3 px-6">
-                ID
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Category
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Title
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Difficulty
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Status
-              </th>
+    <div className="overflow-x-auto relative">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="py-3 px-6">
+              ID
+            </th>
+            <th scope="col" className="py-3 px-6">
+              Category
+            </th>
+            <th scope="col" className="py-3 px-6">
+              Title
+            </th>
+            <th scope="col" className="py-3 px-6">
+              Difficulty
+            </th>
+            <th scope="col" className="py-3 px-6">
+              Status
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr
+              key={item.id}
+              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+            >
+              <td className="py-4 px-6">{item.id}</td>
+              <td className="py-4 px-6">{item.category}</td>
+              <td className="py-4 px-6">{item.title}</td>
+              <td className="py-4 px-6">{item.difficulty}</td>
+              <td className="py-4 px-6">
+                <select
+                  defaultValue={item.status}
+                  className="form-select block w-full mt-1 rounded border-blue-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                >
+                  <option value="Solved">Solved</option>
+                  <option value="Unsolved">Unsolved</option>
+                  <option value="In Progress">In Progress</option>
+                </select>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {data.map((item) => (
-              <tr key={item.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <td className="py-4 px-6">{item.id}</td>
-                <td className="py-4 px-6">{item.category}</td>
-                <td className="py-4 px-6">{item.title}</td>
-                <td className="py-4 px-6">{item.difficulty}</td>
-                <td className="py-4 px-6">{item.status}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      Sheet
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
