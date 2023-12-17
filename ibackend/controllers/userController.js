30075438e18bc114f49be1fda634b575c9c2af1f
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-12-18 01:15:48 +0530

    Get Users

--- ibackend/controllers/userController.js
+++ ibackend/controllers/userController.js
@@ -1,7 +1,7 @@
 // exports.getUsers
 const User = require("../model/user");
 
-exports.postUser = (req,res) => {
+exports.postUser = (req, res) => {
   const { username, email, password } = req.body;
 
   const user = new User({ username, email, password });
@@ -10,10 +10,19 @@ exports.postUser = (req,res) => {
     .save()
     .then((result) => {
       console.log("User Created");
-      res.json({message : 'User has been created'})
+      res.json({ message: "User has been created" });
     })
     .catch((err) => {
       console.log(err);
     });
+};
 
+exports.getUsers = async (req, res) => {
+    try{
+        const users = await User.find();
+        console.log('users', users)
+        res.status(200).json({message:'Get All the Users', data : users})
+    }catch(err){
+        res.status(500).json({message: 'Internal Server Error'})
+    }
 };
--- ibackend/routes/userRoutes.js
+++ ibackend/routes/userRoutes.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const userController = require('../controllers/userController')
 
 // /GET POST => LOGIN
+router.get('/', userController.getUsers)
 
 // /POST CREATE_USER => SIGNUP
 router.post('/signup', userController.postUser)
 
 // /PUT UPDATE_USER => 
+
 
 // /POST OTP
 
